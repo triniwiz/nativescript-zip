206281f08372df0b43244b35121f57df643fd4a1
Sean Perkins <sean@meetmaestro.com>
2017-02-28 01:02:27 -0500

    Adds overwrite functionality to the iOS implementation

--- zip.ios.ts
+++ zip.ios.ts
@@ -1,8 +1,15 @@
 export class Zip {
+
   public static zip() { }
-  public static unzip(archive: string, destination: string) {
+
+  public static unzip(archive: string, destination: string, overwrite?: boolean, password?: string) {
     try {
-      SSZipArchive.unzipFileAtPathToDestination(archive, destination);
+      if (password || overwrite) {
+        SSZipArchive.unzipFileAtPathToDestinationOverwritePasswordError(archive, destination, overwrite, password);
+      }
+      else {
+        SSZipArchive.unzipFileAtPathToDestination(archive, destination);
+      }
     } catch (ex) {
       console.log(ex)
     } finally {
