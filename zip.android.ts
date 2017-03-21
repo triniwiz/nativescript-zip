32fcbbf86074d7e94483dff4bc14aa267fd0d102
Daniel Freiling <dfg@nota.dk>
2017-03-21 16:46:54 +0100

    feat(android): unzipWithProgress

--- zip.android.ts
+++ zip.android.ts
@@ -1,6 +1,43 @@
-declare const net;
+const ProgressMonitor = net.lingala.zip4j.progress.ProgressMonitor;
+
 export class Zip {
+    public static ProgressUpdateRate = 500;
+
     public static zip() {
+    }
+    public static unzipWithProgress(archive: string, destination: string, progressCallback: (progress: number) => void, overwrite?: boolean, password?: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            try {
+                const zipFile = new net.lingala.zip4j.core.ZipFile(archive);
+                zipFile.setRunInThread(true);
+                if (zipFile.isEncrypted() && password) {
+                    zipFile.setPassword(password);
+                }
+                zipFile.extractAll(destination);
+                const monitor = zipFile.getProgressMonitor();
+                const progressInterval = setInterval(() => {
+                    if (monitor.getState() === ProgressMonitor.STATE_BUSY) {
+                        progressCallback(monitor.getPercentDone());
+                    } else {
+                        // Done working
+                        const result = monitor.getResult();
+                        if (result === ProgressMonitor.RESULT_SUCCESS) {
+                            resolve();
+                        } else if (result === ProgressMonitor.RESULT_ERROR) {
+                            reject(monitor.getException() ? monitor.getException().getMessage() : 'error');
+                        } else {
+                            reject('cancelled');
+                        }
+                        clearInterval(progressInterval);
+                    }
+                }, Zip.ProgressUpdateRate);
+            } catch (ex) {
+                console.log(ex);
+                reject(ex);
+            } finally {
+                console.log("done")
+            }
+        });
     }
     public static unzip(archive: string, destination: string, overwrite?: boolean, password?: string) {
         try {
