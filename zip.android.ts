13a1398d7db8fb810970ecff4fbe642c59f10f27
Daniel Freiling <dfg@nota.dk>
2017-03-22 12:10:16 +0100

    chore: fix indentation

--- zip.android.ts
+++ zip.android.ts
@@ -6,56 +6,57 @@
 const ProgressMonitor = net.lingala.zip4j.progress.ProgressMonitor;
 
 export class Zip {
-    public static ProgressUpdateRate = 500;
+  
+  public static ProgressUpdateRate = 100;
 
-    public static zip() {
-    }
-    public static unzipWithProgress(archive: string, destination: string, progressCallback: (progress: number) => void, overwrite?: boolean, password?: string): Promise<any> {
-        return new Promise((resolve, reject) => {
-            try {
-                const zipFile = new net.lingala.zip4j.core.ZipFile(archive);
-                zipFile.setRunInThread(true);
-                if (zipFile.isEncrypted() && password) {
-                    zipFile.setPassword(password);
-                }
-                zipFile.extractAll(destination);
-                const monitor = zipFile.getProgressMonitor();
-                const progressInterval = setInterval(() => {
-                    if (monitor.getState() === ProgressMonitor.STATE_BUSY) {
-                        progressCallback(monitor.getPercentDone());
-                    } else {
-                        // Done working
-                        const result = monitor.getResult();
-                        if (result === ProgressMonitor.RESULT_SUCCESS) {
-                            resolve();
-                        } else if (result === ProgressMonitor.RESULT_ERROR) {
-                            reject(monitor.getException() ? monitor.getException().getMessage() : 'error');
-                        } else {
-                            reject('cancelled');
-                        }
-                        clearInterval(progressInterval);
-                    }
-                }, Zip.ProgressUpdateRate);
-            } catch (ex) {
-                console.log(ex);
-                reject(ex);
-            } finally {
-                console.log("done")
-            }
-        });
-    }
-    public static unzip(archive: string, destination: string, overwrite?: boolean, password?: string) {
-        try {
-            const zipFile = new net.lingala.zip4j.core.ZipFile(archive);
-            zipFile.setRunInThread(true);
-            if (zipFile.isEncrypted() && password) {
-                zipFile.setPassword(password);
-            }
-            zipFile.extractAll(destination);
-        } catch (ex) {
-            console.log(ex);
-        } finally {
-            console.log("done")
+  public static zip() {
+  }
+
+  public static unzipWithProgress(archive: string, destination: string, progressCallback: (progressPercent) => void, overwrite?: boolean, password?: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      try {
+        const zipFile = new net.lingala.zip4j.core.ZipFile(archive);
+        zipFile.setRunInThread(true);
+        if (zipFile.isEncrypted() && password) {
+          zipFile.setPassword(password);
         }
+        zipFile.extractAll(destination);
+        const monitor = zipFile.getProgressMonitor();
+        const progressInterval = setInterval(() => {
+          if (monitor.getState() === ProgressMonitor.STATE_BUSY) {
+            if (progressCallback) progressCallback(monitor.getPercentDone());
+          } else {
+            // Done working
+            const result = monitor.getResult();
+            if (result === ProgressMonitor.RESULT_SUCCESS) {
+              resolve();
+            } else if (result === ProgressMonitor.RESULT_ERROR) {
+              reject(monitor.getException() ? monitor.getException().getMessage() : 'error');
+            } else {
+              reject('cancelled');
+            }
+            clearInterval(progressInterval);
+          }
+        }, Zip.ProgressUpdateRate);
+      } catch (ex) {
+        console.log(ex);
+        reject(ex);
+      }
+    });
+  }
+  
+  public static unzip(archive: string, destination: string, overwrite?: boolean, password?: string) {
+    try {
+      const zipFile = new net.lingala.zip4j.core.ZipFile(archive);
+      zipFile.setRunInThread(true);
+      if (zipFile.isEncrypted() && password) {
+        zipFile.setPassword(password);
+      }
+      zipFile.extractAll(destination);
+    } catch (ex) {
+      console.log(ex);
+    } finally {
+      console.log("done")
     }
+  }
 }
