05bd06c3219cb0ea62d14d072a54338556f49e70
Daniel Freiling <dfg@nota.dk>
2017-03-22 16:46:15 +0100

    chore: update demo

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -14,12 +14,14 @@ export function pageLoaded(args: observable.EventData) {
     page.bindingContext = model = new HelloWorldModel();
 }
 
+const dest = fs.Folder.fromPath(fs.path.join(fs.knownFolders.temp().path, 'books'));
+
 export function unzip() {
   console.log(`begin unzip`);
-  let appPath = fs.knownFolders.currentApp().path
-  let testZipFile = fs.path.join(appPath, 'test.zip');
+  let appPath = fs.knownFolders.currentApp().path;
+  let testZipFile = fs.path.join(appPath, 'test_70mb.zip');
   // Zip.unzip(testZipFile, appPath);
-  Zip.unzipWithProgress(testZipFile, appPath, onZipProgress, true)
+  Zip.unzipWithProgress(testZipFile, dest.path, onZipProgress, true)
     .then(() => {
       console.log(`unzip succesfully completed`);
     })
@@ -29,7 +31,7 @@ export function unzip() {
 }
 
 export function showFiles() {
-    traceFolderTree(fs.knownFolders.currentApp(), 1);
+    traceFolderTree(dest, 2);
 }
 
 function onZipProgress(percent: number) {
@@ -39,14 +41,18 @@ function onZipProgress(percent: number) {
 }
 
 function traceFolderTree(folder: fs.Folder, maxDepth: number = 3, depth: number = 0) {
-  let whitespace = new Array(depth + 1).join('  ');
-  console.log(`${whitespace}${fs.Folder.fromPath(folder.path).name}`);
-  folder.eachEntity((ent) => {
-    if (fs.Folder.exists(ent.path) && depth < maxDepth) {
-      traceFolderTree(fs.Folder.fromPath(ent.path), maxDepth, depth + 1);
-    } else {
-      console.log(`${whitespace}- ${ent.name}`);
-    }
-    return true;
-  });
+  try {
+    let whitespace = new Array(depth + 1).join('  ');
+    console.log(`${whitespace}${fs.Folder.fromPath(folder.path).name}`);
+    folder.eachEntity((ent) => {
+      if (fs.Folder.exists(ent.path) && depth < maxDepth) {
+        traceFolderTree(fs.Folder.fromPath(ent.path), maxDepth, depth + 1);
+      } else {
+        console.log(`${whitespace}- ${ent.name}`);
+      }
+      return true;
+    });
+  } catch(err) {
+    console.log(`err tracing tree: ${err}`);
+  }
 }
