78c0f4a0586de7c13996fce8e0d20de9f8aeeb36
Daniel Freiling <dfg@nota.dk>
2017-03-22 13:28:18 +0100

    feat(iOS): unzip on background thread

--- zip.ios.ts
+++ zip.ios.ts
@@ -1,37 +1,66 @@
+import * as fs from 'file-system';
+
+declare interface TNSAsync {
+  async(nativeClass: any, args: any | any[]): Promise<any>;
+}
+
+// Undocumented but reliable way to call native iOS APIs asynchronously
+// See: https://github.com/NativeScript/NativeScript/issues/1673#issuecomment-190658780
+function doNativeAsync(nativeClass: any, funcRef: any, args: any): Promise<any> {
+  return (<TNSAsync>funcRef).async(nativeClass, args);
+}
+function doOnMainThread(func: any) {
+  Promise.resolve().then(() => {
+    func();
+  });
+}
+
 export class Zip {
 
   public static zip() { }
 
   public static unzipWithProgress(archive: string, destination: string, progressCallback: (progressPercent) => void, overwrite?: boolean, password?: string): Promise<any> {
-    let lastProgressPercent: number;
     return new Promise((resolve, reject) => {
-      const progressHandler = (entry: string, zipFileInfo: any, entryNumber: number, total: number) => {
+
+      if (!fs.File.exists(archive)) {
+        return reject(`File does not exist, invalid archive path: ${archive}`);
+      }
+
+      let lastProgressPercent: number;
+      const progressHandler = (entry: string, zipFileInfo: any, entryNumber: number, entriesTotal: number) => {
         // console.log(`progressHandler: entry=${entry},entryNumber=${entryNumber},total=${total}`);
-        if (progressCallback && total > 0) {
-          // Throttle progress callbacks to client somewhat
-          const percent = entryNumber / total * 100;
-          if (!lastProgressPercent || percent - lastProgressPercent >= 1) {
+        if (progressCallback && entriesTotal > 0) {
+          // Throttle progress callbacks somewhat
+          const percent = Math.floor(entryNumber / entriesTotal * 100);
+          if (percent != lastProgressPercent) {
             lastProgressPercent = percent;
-            progressCallback(Math.floor(percent));
+            doOnMainThread(() => {
+              progressCallback(percent);
+            });
           }
         }
       };
+
       const completionHandler = (path: string, succeeded: boolean, err: NSError) => {
         // console.log(`completionHandler: path=${path},succeeded=${succeeded},err=${err}`);
-        if (succeeded) {
-          resolve();
-        } else {
-          reject(err ? err.debugDescription : 'error');
-        }
+
+        doOnMainThread(() => {
+          if (succeeded) {
+              resolve(path);
+          } else {
+              reject(err ? err.debugDescription : 'error');
+          }
+        });
       };
+
       if (SSZipArchive.isFilePasswordProtectedAtPath(archive) && password) {
-        SSZipArchive.unzipFileAtPathToDestinationOverwritePasswordProgressHandlerCompletionHandler(
-          archive, destination, overwrite, password, progressHandler, completionHandler
+        doNativeAsync(SSZipArchive, SSZipArchive.unzipFileAtPathToDestinationOverwritePasswordProgressHandlerCompletionHandler,
+          [archive, destination, overwrite, password, progressHandler, completionHandler]
         );
       } else {
-        SSZipArchive.unzipFileAtPathToDestinationProgressHandlerCompletionHandler(
-          archive, destination, progressHandler, completionHandler
-        )
+        doNativeAsync(SSZipArchive, SSZipArchive.unzipFileAtPathToDestinationProgressHandlerCompletionHandler,
+          [archive, destination, progressHandler, completionHandler]
+        );
       }
     });
   }
