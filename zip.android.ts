8146d5f4c0edb667a1ab76fbd96cd6eb97c6e336
Osei Fortune <fortune.osei@yahoo.com>
2017-05-16 12:09:59 -0400

    Return Promise

--- zip.android.ts
+++ zip.android.ts
@@ -8,58 +8,59 @@ const ProgressMonitor = net.lingala.zip4j.progress.ProgressMonitor;
 
 export class Zip {
 
-  public static ProgressUpdateRate = 100;
+    public static ProgressUpdateRate = 100;
 
-  public static zip() {
-  }
-
-  public static unzipWithProgress(archive: string, destination: string, progressCallback: (progressPercent) => void, overwrite?: boolean, password?: string): Promise<any> {
-    if (!fs.File.exists(archive)) {
-      return Promise.reject(`File does not exist, invalid archive path: ${archive}`);
+    public static zip() {
     }
-    return new Promise((resolve, reject) => {
-      try {
-        const zipFile = new net.lingala.zip4j.core.ZipFile(archive);
-        zipFile.setRunInThread(true);
-        if (zipFile.isEncrypted() && password) {
-          zipFile.setPassword(password);
+
+    public static unzipWithProgress(archive: string, destination: string, progressCallback: (progressPercent) => void, overwrite?: boolean, password?: string): Promise<any> {
+        if (!fs.File.exists(archive)) {
+            return Promise.reject(`File does not exist, invalid archive path: ${archive}`);
         }
-        zipFile.extractAll(destination);
-        const monitor = zipFile.getProgressMonitor();
-        const progressInterval = setInterval(() => {
-          if (monitor.getState() === ProgressMonitor.STATE_BUSY) {
-            if (progressCallback) progressCallback(monitor.getPercentDone());
-          } else {
-            // Done working
-            const result = monitor.getResult();
-            if (result === ProgressMonitor.RESULT_SUCCESS) {
-              resolve();
-            } else if (result === ProgressMonitor.RESULT_ERROR) {
-              reject(monitor.getException() ? monitor.getException().getMessage() : 'error');
-            } else {
-              reject('cancelled');
+        return new Promise((resolve, reject) => {
+            try {
+                const zipFile = new net.lingala.zip4j.core.ZipFile(archive);
+                zipFile.setRunInThread(true);
+                if (zipFile.isEncrypted() && password) {
+                    zipFile.setPassword(password);
+                }
+                zipFile.extractAll(destination);
+                const monitor = zipFile.getProgressMonitor();
+                const progressInterval = setInterval(() => {
+                    if (monitor.getState() === ProgressMonitor.STATE_BUSY) {
+                        if (progressCallback) progressCallback(monitor.getPercentDone());
+                    } else {
+                        // Done working
+                        const result = monitor.getResult();
+                        if (result === ProgressMonitor.RESULT_SUCCESS) {
+                            resolve();
+                        } else if (result === ProgressMonitor.RESULT_ERROR) {
+                            reject(monitor.getException() ? monitor.getException().getMessage() : 'error');
+                        } else {
+                            reject('cancelled');
+                        }
+                        clearInterval(progressInterval);
+                    }
+                }, Zip.ProgressUpdateRate);
+            } catch (ex) {
+                reject(ex);
             }
-            clearInterval(progressInterval);
-          }
-        }, Zip.ProgressUpdateRate);
-      } catch (ex) {
-        reject(ex);
-      }
-    });
-  }
-  
-  public static unzip(archive: string, destination: string, overwrite?: boolean, password?: string) {
-    try {
-      const zipFile = new net.lingala.zip4j.core.ZipFile(archive);
-      zipFile.setRunInThread(true);
-      if (zipFile.isEncrypted() && password) {
-        zipFile.setPassword(password);
-      }
-      zipFile.extractAll(destination);
-    } catch (ex) {
-      console.log(ex);
-    } finally {
-      console.log("done")
+        });
     }
-  }
+
+    public static unzip(archive: string, destination: string, overwrite?: boolean, password?: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            try {
+                const zipFile = new net.lingala.zip4j.core.ZipFile(archive);
+                zipFile.setRunInThread(true);
+                if (zipFile.isEncrypted() && password) {
+                    zipFile.setPassword(password);
+                }
+                zipFile.extractAll(destination);
+                resolve();
+            } catch (ex) {
+                reject(ex);
+            }
+        });
+    }
 }
