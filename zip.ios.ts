2f4a136bdbfc72b94eb1b8be78c1d35a4abde46e
Osei Fortune <fortune.osei@yahoo.com>
2017-12-19 18:07:10 -0400

    Make worker webpackable

--- zip.ios.ts
+++ zip.ios.ts
@@ -1,53 +1,81 @@
 import * as fs from 'file-system';
 
 export class Zip {
+  public static zip() {}
 
-    public static zip() {
-    }
+  public static unzipWithProgress(
+    archive: string,
+    destination: string,
+    progressCallback: (progressPercent) => void,
+    overwrite?: boolean,
+    password?: string
+  ): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if (!fs.File.exists(archive)) {
+        return reject(`File does not exist, invalid archive path: ${archive}`);
+      }
 
-    public static unzipWithProgress(archive: string, destination: string, progressCallback: (progressPercent) => void, overwrite?: boolean, password?: string): Promise<any> {
-        return new Promise((resolve, reject) => {
+      let worker;
+      if (global['TNS_WEBPACK']) {
+        const WorkerScript = require('nativescript-worker-loader!./zip-worker-ios');
+        worker = new WorkerScript();
+      } else {
+        worker = new Worker('./zip-worker-ios');
+      }
+      worker.postMessage(<ZipRequest>{
+        action: 'unzip',
+        archive,
+        destination,
+        overwrite,
+        password
+      });
+      worker.onmessage = msg => {
+        // console.log(`Received worker callback: ${JSON.stringify(msg)}`);
+        if (msg.data.progress != undefined) {
+          progressCallback(msg.data.progress);
+        } else if (msg.data.result != undefined) {
+          if (msg.data.result) {
+            resolve();
+          } else {
+            reject(msg.data.err);
+          }
+        } else {
+          reject('zip-worker-ios failed');
+        }
+      };
+      worker.onerror = err => {
+        console.log(
+          `An unhandled error occurred in worker: ${err.filename}, line: ${
+            err.lineno
+          }`
+        );
+        reject(err.message);
+      };
+    });
+  }
 
-            if (!fs.File.exists(archive)) {
-                return reject(`File does not exist, invalid archive path: ${archive}`);
-            }
-
-            var worker = new Worker('./zip-worker-ios');
-            worker.postMessage(<ZipRequest>{ action: 'unzip', archive, destination, overwrite, password });
-            worker.onmessage = (msg) => {
-                // console.log(`Received worker callback: ${JSON.stringify(msg)}`);
-                if (msg.data.progress != undefined) {
-                    progressCallback(msg.data.progress);
-                } else if (msg.data.result != undefined) {
-                    if (msg.data.result) {
-                        resolve();
-                    } else {
-                        reject(msg.data.err);
-                    }
-                } else {
-                    reject('zip-worker-ios failed');
-                }
-            };
-            worker.onerror = (err) => {
-                console.log(`An unhandled error occurred in worker: ${err.filename}, line: ${err.lineno}`);
-                reject(err.message);
-            };
-        });
-    }
-
-    public static unzip(archive: string, destination: string, overwrite?: boolean, password?: string): Promise<any> {
-        return new Promise((resolve, reject) => {
-            try {
-                if (password || overwrite) {
-                    SSZipArchive.unzipFileAtPathToDestinationOverwritePasswordError(archive, destination, overwrite, password);
-                }
-                else {
-                    SSZipArchive.unzipFileAtPathToDestination(archive, destination);
-                }
-                resolve()
-            } catch (ex) {
-                reject(ex)
-            }
-        });
-    }
+  public static unzip(
+    archive: string,
+    destination: string,
+    overwrite?: boolean,
+    password?: string
+  ): Promise<any> {
+    return new Promise((resolve, reject) => {
+      try {
+        if (password || overwrite) {
+          SSZipArchive.unzipFileAtPathToDestinationOverwritePasswordError(
+            archive,
+            destination,
+            overwrite,
+            password
+          );
+        } else {
+          SSZipArchive.unzipFileAtPathToDestination(archive, destination);
+        }
+        resolve();
+      } catch (ex) {
+        reject(ex);
+      }
+    });
+  }
 }
