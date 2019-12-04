40e8fc5454a55826ff2b59b69d26192bc0f60b05
Osei Fortune <fortune.osei@yahoo.com>
2019-12-04 01:12:13 -0400

    chore(): update demo

--- demo/app/tests/tests.js
+++ demo/app/tests/tests.js
@@ -1,12 +1,46 @@
-var Zip = require("nativescript-zip").Zip;
-var zip = new Zip();
+var Zip = require('nativescript-zip').Zip;
+var fs = require('tns-core-modules/file-system');
+var testData = 'Testing testing 123';
+var testPath = function () {
+	return fs.path.join(fs.knownFolders.documents().path, 'zip_test');
+};
+var testFile = `${Date.now()}.txt`;
+var output;
+describe('Zip functions', function () {
+	it('creates a file then add file to zip', function () {
+		const testFilePath = fs.path.join(testPath(), testFile);
+		fs.File.fromPath(testFilePath).writeTextSync(testData, function (error) {
+			fail(error);
+		});
+		return Zip.zip({
+			directory: testPath(),
+		})
+			.then(function (archive) {
+				output = archive;
+				expect(archive).toBeDefined();
+				expect(fs.File.exists(archive)).toBe(true);
+				expect(fs.File.fromPath(archive).size).toBeGreaterThan(0);
+			}).catch(function (error) {
+				fail(error);
+			});
+	});
 
-describe("greet function", function() {
-    it("exists", function() {
-        expect(zip.greet).toBeDefined();
-    });
+	it('extract zip then reads file created earlier', function () {
+		expect(output).toBeDefined();
+		return Zip.unzip({
+			archive: output
+		})
+			.then(function (path) {
+				expect(path).toBeDefined();
+				expect(fs.Folder.exists(path)).toBe(true);
+				var file = fs.path.join(path, 'zip_test', testFile);
+				var text = fs.File.fromPath(file).readTextSync(function (error) {
+					fail(error);
+				});
+				expect(text).toEqual(testData);
+			}).catch(function (error) {
+				fail(error);
+			})
+	});
 
-    it("returns a string", function() {
-        expect(zip.greet()).toEqual("Hello, NS");
-    });
 });
